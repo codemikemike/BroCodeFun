df13f717a8a834096d82c08de8227f44d4a548cb
Michael Kragh <mikr71394@edu.ucl.dk>
2025-08-02 17:58:36 +0200

    add variables declaration and initialization plus Console.WriteLines

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
-﻿namespace BroCode
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace BroCode
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I like pizzadd!");
+            Console.WriteLine("Are you a computer nerd!?");
             // Text color
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("This is cyan!");
+            Console.WriteLine("Your face is cyan color from the screen!");
             Console.ResetColor();
             // Loop through all colors:
             /*foreach (ConsoleColor color in Enum.GetValues(typeof(ConsoleColor)))
@@ -34,7 +36,7 @@
             Console.SetCursorPosition(10, 5);
             Console.WriteLine("Hello from (10,5)!");
             // Console Title
-            Console.Title = "BroCode Console App";
+            Console.Title = "CodeMike Console App";
 
             // Matrix Effect (Mini Version) - commented out properly
             /*
@@ -50,12 +52,32 @@
             // Clear Screen Dramatically and Play Sound
             Console.Clear();
             Console.Beep(800, 1000);
-            Console.WriteLine("Everything's gone!");
+            Console.WriteLine("Releasing your infos!");
 
             /*
              *This is a simple console application that demonstrates various features of the C# console.
              * And it is a great way to learn about console applications in C#.
              */
+
+            int x; // declaration
+            x = 5; // initialization
+
+            int y = 10; // declaration and initialization
+
+            int z = x + y; // addition
+
+            int age = 21; // variable declaration (whole integer)
+            double height = 5.9; // variable declaration (decimal number)
+            bool alive = true; // variable declaration (boolean)
+            char symbol = 'M'; // variable declaration (character) (Could also be feks. @)
+            string name = "Code Mike"; // variable declaration (string)
+
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Your height is " + height + "cm");
+            Console.WriteLine("Are you alive? " + alive);
+            Console.WriteLine("Your symbol is: " + symbol);
+
+
 
             // Wait for user input before closing
             Console.ReadKey();
