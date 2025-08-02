919184ef6154e13325ebc2e9d3505b670a7930f4
Michael Kragh <mikr71394@edu.ucl.dk>
2025-08-02 18:15:55 +0200

    add convert datatypes (type casting)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace BroCode
 {
@@ -77,6 +78,34 @@ namespace BroCode
             Console.WriteLine("Are you alive? " + alive);
             Console.WriteLine("Your symbol is: " + symbol);
 
+            //Constants = immutable values that cannot be changed adds security and performance to programs
+            const double PI = 3.14159; // constant declaration
+            /*PI = 420; This line will cause a compile-time error because PI is a constant and cannot be changed*/
+
+            // Type Casting = converting one data type to another
+            // useful when we accept user input as a string and need to convert it to an integer or double
+            //different data types can do different things, so we need to convert them to the right type
+
+            double a = 3.14;
+            int b = Convert.ToInt32(a);
+
+            int c = 5;
+            double d = Convert.ToDouble(c);
+
+            int e = 10;
+            String f = Convert.ToString(e);
+
+            String g = "$";
+            char h = Convert.ToChar(g);
+
+            String i = "true";
+            bool j = Convert.ToBoolean(i);
+
+            Console.WriteLine(b.GetType());
+            Console.WriteLine(d.GetType());
+            Console.WriteLine(f.GetType());
+            Console.WriteLine(h.GetType());
+            Console.WriteLine(j.GetType());
 
 
             // Wait for user input before closing
